d1726428d0137f932df5e29e6a06393eaa235248
cj89898 <cj89898@gmail.com>
2020-03-30 00:14:11 -0500

    Cleaned up code

--- src/net/cjservers/givemesoup/GiveMeSoup.java
+++ src/net/cjservers/givemesoup/GiveMeSoup.java
@@ -8,6 +8,8 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.md_5.bungee.api.ChatColor;
+
 public class GiveMeSoup extends JavaPlugin {
 	
 	@Override
@@ -17,21 +19,21 @@ public class GiveMeSoup extends JavaPlugin {
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (sender instanceof Player) {
-			Player p = (Player) sender;
-			if (cmd.getName().equalsIgnoreCase("givemesoup")) {
-				if (p.hasPermission("givemesoup.access")) {
-					PlayerInventory inv = p.getInventory();
-					for (int i = 0; i < inv.getSize(); ++i) {
-						if (inv.getItem(i) == null) {
-							inv.setItem(i, new ItemStack(Material.MUSHROOM_SOUP));
-						}
-					}
-					return true;
-				}
-			}
+		if (!(sender instanceof Player))
+			return false;
+		
+		Player p = (Player) sender;
+		if (!p.hasPermission("givemesoup.access")) {
+			p.sendMessage(ChatColor.RED + "No permission");
+			return false;
 		}
-		return false;
+		
+		PlayerInventory inv = p.getInventory();
+		p.getInventory().forEach(item -> {
+			if (item == null)
+				inv.addItem(new ItemStack(Material.MUSHROOM_SOUP));
+		});
+		return true;
 	}
 	
 }
